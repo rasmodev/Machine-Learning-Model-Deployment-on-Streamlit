9ab5bb329ef04f4b9d26db537715283f5b221bb8
Rasmo Wanyama <rasmowanyama@gmail.com>
2023-08-22 19:22:55 +0300

    Streamlit App

--- src/app.py
+++ src/app.py
@@ -5,71 +5,74 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import pickle
 from tabulate import tabulate
 
-# Load the best model and relevant data
-with open('model_and_key_components.pkl', 'rb') as model_file:
-    loaded_data = pickle.load(model_file)
-    model = loaded_data['model']
-    unique_category_values = loaded_data['unique_category_values']
-    min_oil_price = loaded_data['min_oil_price']
-    max_oil_price = loaded_data['max_oil_price']
-    best_hyperparameters = loaded_data['best_hyperparameters']
-    encoder = loaded_data['encoder']
-    scaler = loaded_data['scaler']
 
+# Loading the trained model and components
+encoder = pickle.load(open('categorical_encoder.pkl', 'rb'))
+numerical_scaler = pickle.load(open('numerical_scaler.pkl', 'rb'))
+model = pickle.load(open('best_rf_model.pkl', 'rb'))
 
-# Create a Streamlit app
-st.title('Sales Prediction App')
+st.image("https://ultimahoraec.com/wp-content/uploads/2021/03/dt.common.streams.StreamServer-768x426.jpg")
+st.title("Sales Prediction App")
 
-# Add a sidebar with widgets for the input features
-st.sidebar.title('Input Features')
+st.caption("This app predicts sales patterns of Corporation Favorita over time in different stores in Ecuador based on the inputs.")
 
-store_nbr = st.sidebar.number_input('Store Number', min_value=1, max_value=9999)
-family = st.sidebar.selectbox('Product Family', unique_category_values['family'])
-city = st.sidebar.selectbox('City', unique_category_values['city'])
-holiday_type = st.sidebar.selectbox('Holiday Type', unique_category_values['holiday_type'])
+# Create the input fields
+input_df = {}
+col1, col2 = st.columns(2)
+with col1:
+    input_df['store_nbr'] = st.slider("Store Number", 0, 54)
+    input_df['family'] = st.selectbox("Product Family", ['AUTOMOTIVE', 'BEAUTY', 'CELEBRATION', 'CLEANING', 'CLOTHING', 'FOODS', 
+                                            'GROCERY', 'HARDWARE', 'HOME', 'LADIESWEAR', 'LAWN AND GARDEN', 'LIQUOR,WINE,BEER', 
+                                            'PET SUPPLIES', 'STATIONERY'])
+    input_df['onpromotion'] = st.number_input("Number of Items on Promotion", step=1)
+    input_df['city'] = st.selectbox("City", ['Ambato', 'Babahoyo', 'Cayambe', 'Cuenca', 'Daule', 'El Carmen', 'Esmeraldas',
+                                         'Guaranda', 'Guayaquil', 'Ibarra', 'Latacunga', 'Libertad', 'Loja', 'Machala', 'Manta',
+                                         'Playas', 'Puyo', 'Quevedo', 'Quito', 'Riobamba', 'Salinas', 'Santo Domingo'])
+    input_df['cluster'] = st.selectbox("Cluster", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+    input_df['transactions'] = st.number_input("Number of Transactions")
+    input_df['holiday_type'] = st.selectbox("Holiday Type", ['Additional', 'Bridge', 'Event', 'Holiday', 'Transfer'])    
 
-onpromotion = st.sidebar.checkbox('On Promotion')
-if onpromotion:
-    onpromotion_str = 'True'
-else:
-    onpromotion_str = 'False'
-cluster = st.sidebar.number_input('Customer Cluster', min_value=1, max_value=10)
-transactions = st.sidebar.number_input('Number of Transactions', min_value=0)
-dcoilwtico = st.sidebar.number_input('Daily Oil Price', min_value=0)
+with col2:
+    input_df['dcoilwtico'] = st.slider("Crude Oil Price", min_value=1.00, max_value=100.00, step=0.1)  
+    input_df['year'] = st.number_input("Year", step=1)
+    input_df['month'] = st.slider("Month", 1, 12)
+    input_df['day'] = st.slider("Day", 1, 31)
+    input_df['day_of_week'] = st.number_input("Day of Week, 0=Sun and 6=Sat", step=1)
 
-# Use the model to predict the sales
-input_data = {
-    'store_nbr': store_nbr,
-    'family': family,
-    'city': city,
-    'holiday_type': holiday_type,
-    'onpromotion': onpromotion_str,
-    'cluster': cluster,
-    'transactions': transactions,
-    'dcoilwtico': dcoilwtico
-}
+# Create a button to make a prediction
+if st.button("Predict Sales"):
+    # Convert the input data to a pandas DataFrame
+    input_data = pd.DataFrame([input_df])
 
-# Convert the input data to a Pandas DataFrame
-input_data_df = pd.DataFrame(input_data)
+    # Scale the Numerical Columns
+    num_cols = ['transactions', 'dcoilwtico']
+    
+    # Scale the numerical columns
+    input_data[num_cols] = numerical_scaler.transform(input_data[num_cols])
 
-# Check if the input data is empty
-if not input_data_df.empty:
+    # Encode the categorical columns
+    cat_cols = ['family', 'city', 'holiday_type']
 
-    # Get the feature importances
-    feature_importances = model.feature_importances_
+    # Transform the categorical columns using the fitted encoder    
+    one_hot_encoded_data = encoder.fit_transform(input_data[cat_cols])
 
-    # Keep only the features with importance above a threshold
-    seen_features = set(feature_importances[feature_importances > 0.0])
-    seen_features = [feature for feature in seen_features if feature in input_data_df.columns]
+    # Create column names for the one-hot encoded data
+    column_names = encoder.get_feature_names_out(cat_cols)
+    
+    # Convert the one-hot encoded data to a DataFrame
+    final_df = pd.DataFrame(one_hot_encoded_data.toarray(), columns=column_names)
+    
+    # Concatenate the original dataframe with the one-hot encoded data
+    final_df = pd.concat([input_data, final_df], axis=1)
 
-    # Filter the input data to keep only the seen features
-    input_data_df = input_data_df[seen_features]
+    # Drop the original categorical columns
+    final_df.drop(cat_cols, axis=1, inplace=True)
 
-    # Make predictions
-    prediction = model.predict(input_data_df)
+    # Make a prediction
+    prediction = model.predict(final_df)[0]
 
     # Display the prediction
-    st.write({'Predicted Sales': prediction})
+    st.write(f"The predicted sales are: {prediction}.")
+    st.table(final_df)
 
-else:
-    st.write('Please enter some input data.')
+st.balloons()
